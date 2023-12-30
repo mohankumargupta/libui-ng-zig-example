fdf838fc394e6233bcf4b7d01bc922e11a76e82d
Mohan Gupta <mohangupta@live.com>
2023-12-30 19:40:13 +1100

    i will take that

--- src/main.zig
+++ src/main.zig
@@ -7,12 +7,21 @@ pub fn on_closing(_: *ui.Window, _: ?*void) ui.Window.ClosingAction {
 }
 
 const Application = struct {
-    vbox: *ui.Box = undefined,
-    tab: *ui.Tab = undefined,
-    form: *ui.Form = undefined,
+    main_window: *ui.Window = undefined,
+    label: *ui.Label = undefined,
+
+    //vbox: *ui.Box = undefined,
+    //group: *ui.Group = undefined,
+    //form: *ui.Form = undefined,
 
     const Self = @This();
 };
+
+pub fn uploadClicked(_: *ui.Button, app: ?*Application) void {
+    const result = app.?.main_window.uiOpenFolder();
+    const moo: [*:0]const u8 = @ptrCast(@alignCast(result.?));
+    app.?.label.SetText(moo);
+}
 
 pub fn main() !void {
     var init_data = ui.InitData{
@@ -25,19 +34,49 @@ pub fn main() !void {
     };
     defer ui.Uninit();
 
-    const main_window = try ui.Window.New("VSCode Portable Dowloader", 1000, 750, .hide_menubar);
+    const main_window = try ui.Window.New("VSCode Portable Dowloader", 240, 200, .hide_menubar);
     main_window.SetMargined(true);
 
     const vbox = try ui.Box.New(.Vertical);
-    const tab = try ui.Tab.New();
-    const download = try ui.Label.New("Hello world");
-    const update = try ui.Label.New("Goodbye world");
-    tab.Append("Download", download.as_control());
-    tab.Append("Update", update.as_control());
-    vbox.Append(tab.as_control(), .stretch);
+    const group = try ui.Group.New("Download VSCode Portable Edition");
+    const form = try ui.Form.New();
+    const entry = try ui.Entry.New(.Entry);
+    const group2 = try ui.Group.New("Update Existing VSCode");
+    const form2 = try ui.Form.New();
+    const choosefolder = try ui.Button.New("Choose Folder");
+    const downloadButton = try ui.Button.New("Download");
+    const label = try ui.Label.New("moo");
+    const label2 = try ui.Label.New("boo");
+    label2.SetText("pop");
 
-    const app: Application = .{ .vbox = vbox, .tab = tab };
-    _ = app;
+    const vbox2 = try ui.Box.New(.Vertical);
+    //group.SetMargined(bool);
+    ui.Group.uiGroupSetMargined(group, 1);
+    form.Append("VSCode Folder", entry.as_control(), .dont_stretch);
+    form.Append("", downloadButton.as_control(), .dont_stretch);
+    form.Append("Result:", label.as_control(), .dont_stretch);
+    form.SetPadded(true);
+    group.SetChild(form.as_control());
+    vbox2.Append(group.as_control(), .dont_stretch);
+    form2.Append("", choosefolder.as_control(), .dont_stretch);
+    form2.SetPadded(true);
+    group2.SetChild(form2.as_control());
+    vbox2.Append(group2.as_control(), .stretch);
+
+    const vbox3 = try ui.Box.New(.Vertical);
+    const grid = try ui.Grid.New();
+    const vscodeDownload = try ui.Button.New("Download VSCode");
+    const vscodeUpload = try ui.Button.New("Update VSCode");
+    var app: Application = .{ .main_window = main_window, .label = label2 };
+    vscodeUpload.OnClicked(Application, uploadClicked, &app);
+    grid.Append(vscodeDownload.as_control(), 0, 0, 1, 1, 0, .Fill, 0, .Fill);
+    grid.Append(vscodeUpload.as_control(), 0, 1, 1, 1, 0, .Fill, 0, .Fill);
+    grid.Append(label2.as_control(), 0, 2, 1, 1, 0, .Fill, 0, .Fill);
+    //grid.Append()
+    grid.SetPadded(true);
+    vbox3.Append(grid.as_control(), .stretch);
+
+    vbox.Append(vbox3.as_control(), .stretch);
 
     main_window.SetChild(vbox.as_control());
     main_window.as_control().Show();
